e2d6da255d1e61bd593c4c7f788ae6d13797bab8
Daniel Voros <daniel.voros@gmail.com>
2023-11-18 17:03:42 +0100

    aoc 2021 / 6 / 1

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,31 @@
-use std::{collections::HashMap, fmt::Display, ops::Add};
+use atoi::atoi;
+use nom::{combinator::map_opt, IResult};
 
-use crate::util::read_lines;
-use regex::Regex;
+pub fn main() {
+    let mut fish = vec![];
+    include_bytes!("../input")
+        .split(|b| *b == b',')
+        .map(|entry| num(entry).unwrap().1)
+        .for_each(|x| fish.push(x));
 
-pub mod util;
-
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
-struct Point {
-    x: i32,
-    y: i32,
+    let rounds_target = 80;
+    // println!("{:?}", fish);
+    for round in 1..=rounds_target {
+        let mut new_fish = vec![];
+        for f in fish.iter_mut() {
+            *f -= 1;
+            if *f == -1 {
+                *f = 6;
+                new_fish.push(8);
+            }
+        }
+        fish.append(&mut new_fish);
+        println!("round {round} done")
+        // println!("{:?}", fish);
+    }
+    println!("{}", fish.len());
 }
 
-impl Add<Point> for Point {
-    type Output = Point;
-    fn add(self, rhs: Self) -> Self::Output {
-        Point {
-            x: self.x + rhs.x,
-            y: self.y + rhs.y,
-        }
-    }
-}
-
-impl Point {
-    fn from_i32s(x: i32, y: i32) -> Point {
-        Point { x, y }
-    }
-
-    fn points_between(&self, other: &Point) -> Vec<Point> {
-        let mut res = Vec::new();
-        let delta = Point::from_i32s((other.x - self.x).signum(), (other.y - self.y).signum());
-        let mut p = self.clone();
-        res.push(p);
-        while p != *other {
-            p = p + delta;
-            res.push(p);
-        }
-        return res;
-    }
-
-    fn is_diagonal(&self, other: &Point) -> bool {
-        return self.x != other.x && self.y != other.y;
-    }
-}
-
-impl Display for Point {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}, {}", self.x, self.y)
-    }
-}
-
-fn main() -> Result<(), std::io::Error> {
-    let lines = read_lines("./input")?;
-    let re = Regex::new("([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)").unwrap();
-
-    let mut board: HashMap<Point, u32> = HashMap::new();
-    for line in lines {
-        let line = line?;
-        let captures = re.captures(line.as_str()).unwrap();
-
-        let nums: Vec<i32> = (1..5)
-            .map(|x| captures.get(x).unwrap().as_str().parse::<i32>().unwrap())
-            .collect();
-
-        let from = Point::from_i32s(nums[0], nums[1]);
-        let to = Point::from_i32s(nums[2], nums[3]);
-        // println!("{from} -> {to}");
-
-        let between = from.points_between(&to);
-        for p in between {
-            board.insert(p, board.get(&p).unwrap_or(&0) + 1);
-            // println!(" - {}", p)
-        }
-    }
-
-    let mut sum = 0;
-    for (k, v) in board {
-        if v > 1 {
-            sum += 1;
-        }
-    }
-    println!("answer is: {sum}");
-
-    Ok(())
+fn num(input: &[u8]) -> IResult<&[u8], i32> {
+    map_opt(nom::character::complete::digit1, atoi)(input)
 }
