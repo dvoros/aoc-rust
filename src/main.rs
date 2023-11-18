ab2a88b7eb67dad97a3852255d98f05f8ae9e9fe
Daniel Voros <daniel.voros@gmail.com>
2023-11-18 18:05:14 +0100

    aoc 2021 / 7 / 2

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,42 @@ use atoi::atoi;
 use nom::{combinator::map_opt, IResult};
 
 pub fn main() {
-    let mut fish: [u64; 9] = [0; 9];
+    let mut nums = vec![];
+    let (mut min, mut max) = (u32::MAX, 0);
     include_bytes!("../input")
         .split(|b| *b == b',')
         .map(|entry| num(entry).unwrap().1)
-        .for_each(|x| fish[x] += 1);
-
-    let rounds_target = 256;
-    // println!("{:?}", fish);
-    for round in 1..=rounds_target {
-        let new_fish = fish[0];
-        for i in 0..fish.len() - 1 {
-            fish[i] = fish[i + 1];
+        .for_each(|x| {
+            nums.push(x);
+            if x > max {
+                max = x;
+            }
+            if x < min {
+                min = x
+            }
+        });
+    println!("there are {} values between {min} and {max}", nums.len());
+    let mut min_cost = u32::MAX;
+    for n in min..=max {
+        println!("checking {n}");
+        let c = cost(&nums, n);
+        if c < min_cost {
+            min_cost = c;
         }
-        fish[8] = new_fish;
-        fish[6] += new_fish;
-        println!("round {round} done")
-        // println!("{:?}", fish);
     }
-    let res: u64 = fish.iter().sum();
-    println!("{}", res);
+    println!("min cost is {min_cost}");
 }
 
-fn num(input: &[u8]) -> IResult<&[u8], usize> {
+fn cost(positions: &[u32], target: u32) -> u32 {
+    let mut c = 0;
+    for p in positions {
+        if *p != target {
+            c += (1..=p.abs_diff(target)).sum::<u32>();
+        }
+    }
+    c
+}
+
+fn num(input: &[u8]) -> IResult<&[u8], u32> {
     map_opt(nom::character::complete::digit1, atoi)(input)
 }
