72a4d6ed26f03668e42b15b178c82dd601403d12
Daniel Voros <daniel.voros@gmail.com>
2023-11-20 14:06:20 +0100

    aoc 2021 / 10 / 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use nom::{
     bytes::complete::{tag, take},
     combinator::map_res,
@@ -20,55 +22,43 @@ impl Point {
 }
 
 pub fn main() {
-    let mut mx = parse_lines(include_str!("../input")).unwrap().1;
-
-    let mut sizes = Vec::new();
-    for i in 0..mx.len() {
-        for j in 0..mx[0].len() {
-            if !mx[i][j].found && mx[i][j].value != 9 {
-                let basin_size = basin(&mut mx, i, j);
-                // println!("basin of {basin_size}");
-                sizes.push(basin_size);
-            }
-        }
-    }
-    sizes.sort_unstable();
-    sizes.reverse();
-    let res = sizes[0..3].iter().fold(1, |c, x| c * x);
-    println!("result is {res}");
-}
-
-fn basin(mx: &mut Vec<Vec<Point>>, i: usize, j: usize) -> u32 {
-    mx[i][j].found = true;
-    if mx[i][j].value == 9 {
-        return 0;
-    }
-    let mut from_neighbors = 0;
-    let deltas: [(isize, isize); 4] = [(-1, 0), (0, -1), (1, 0), (0, 1)];
-    for delta in deltas {
-        let r = i as isize + delta.0;
-        if r >= 0 && r < mx.len() as isize {
-            let row = &mx[r as usize];
-            let c = j as isize + delta.1;
-            if c >= 0 && c < row.len() as isize {
-                if !mx[r as usize][c as usize].found {
-                    from_neighbors += basin(mx, r as usize, c as usize);
+    let mut closing = HashMap::new();
+    closing.insert('[', ']');
+    closing.insert('(', ')');
+    closing.insert('{', '}');
+    closing.insert('<', '>');
+    let mut scores = HashMap::new();
+    scores.insert(')', 3);
+    scores.insert(']', 57);
+    scores.insert('}', 1197);
+    scores.insert('>', 25137);
+    let mut syntax_error_score = 0;
+    include_str!("../input").lines().for_each(|line| {
+        // println!("{:?}", line);
+        let mut stack = Vec::new();
+        for ch in line.chars() {
+            match closing.get(&ch) {
+                Some(pair) => {
+                    // Opening
+                    stack.push(pair);
                 }
+                None => match stack.pop() {
+                    // Closing
+                    Some(&last) => {
+                        if last != ch {
+                            // println!("illegal, doesn't match: {ch} != {last}");
+                            syntax_error_score += scores[&ch];
+                            break;
+                        }
+                    }
+                    None => {
+                        // println!("illegal, nothing's open: {ch}");
+                        syntax_error_score += scores[&ch];
+                        break;
+                    }
+                },
             }
         }
-    }
-    1 + from_neighbors
-}
-
-fn from_str(input: &str) -> Result<Point, std::num::ParseIntError> {
-    let value = input.parse::<u8>()?;
-    Ok(Point::new(value))
-}
-
-fn parse_line(input: &str) -> IResult<&str, Vec<Point>> {
-    many1(map_res(take(1usize), from_str))(input)
-}
-
-fn parse_lines(input: &str) -> IResult<&str, Vec<Vec<Point>>> {
-    separated_list1(tag("\n"), parse_line)(input)
+    });
+    println!("final score is {syntax_error_score}")
 }
