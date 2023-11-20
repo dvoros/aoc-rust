6247890e82cac6a468e69bfbd462f0b3867283be
Daniel Voros <daniel.voros@gmail.com>
2023-11-20 14:27:27 +0100

    aoc 2021 / 10 / 2

--- src/main.rs
+++ src/main.rs
@@ -28,14 +28,15 @@ pub fn main() {
     closing.insert('{', '}');
     closing.insert('<', '>');
     let mut scores = HashMap::new();
-    scores.insert(')', 3);
-    scores.insert(']', 57);
-    scores.insert('}', 1197);
-    scores.insert('>', 25137);
-    let mut syntax_error_score = 0;
+    scores.insert(')', 1);
+    scores.insert(']', 2);
+    scores.insert('}', 3);
+    scores.insert('>', 4);
+    let mut all_scores = Vec::new();
     include_str!("../input").lines().for_each(|line| {
         // println!("{:?}", line);
         let mut stack = Vec::new();
+        let mut valid = true;
         for ch in line.chars() {
             match closing.get(&ch) {
                 Some(pair) => {
@@ -47,18 +48,30 @@ pub fn main() {
                     Some(&last) => {
                         if last != ch {
                             // println!("illegal, doesn't match: {ch} != {last}");
-                            syntax_error_score += scores[&ch];
+                            valid = false;
                             break;
                         }
                     }
                     None => {
                         // println!("illegal, nothing's open: {ch}");
-                        syntax_error_score += scores[&ch];
+                        valid = false;
                         break;
                     }
                 },
             }
         }
+        if valid {
+            let mut score: u64 = 0;
+            stack.reverse();
+            for s in stack {
+                score *= 5;
+                score += scores[s];
+            }
+            // println!("score: {score}");
+            all_scores.push(score);
+        }
     });
-    println!("final score is {syntax_error_score}")
+    let count = all_scores.len();
+    let (_, final_score, _) = all_scores.select_nth_unstable(count / 2);
+    println!("final score is {final_score}")
 }
