25e8e3b12b3fa6c981cac5e15790ebeb2c30b49d
Daniel Voros <daniel.voros@gmail.com>
2023-12-04 08:35:42 +0100

    aoc 2023 / 4 / 1

---
+++ 2023/d04p1/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let res: usize = include_str!("../input").trim().lines().map(
+        |line| {
+            let line = line.split_once(":").unwrap().1;
+            let (winning, all) = line.split_once("|").unwrap();
+            let winning: Vec<_> = winning.split_whitespace().collect();
+            let mut count = all.split_whitespace().filter(|n| winning.contains(n)).count();
+            if count > 0 {
+                count -= 1;
+                return (2 as usize).pow(count as u32);
+            } else {
+                return 0;
+            }
+        }
+    ).sum();
+    println!("res: {res}");
+}
