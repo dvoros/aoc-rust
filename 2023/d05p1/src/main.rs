697e2eae03246d25aee5f188cbabd6c5a268778b
Daniel Voros <daniel.voros@gmail.com>
2023-12-05 08:24:51 +0100

    aoc 2023 / 5 / 1

---
+++ 2023/d05p1/src/main.rs
@@ -0,0 +1,47 @@
+fn main() {
+    let mut parts = include_str!("../input").split("\n\n");
+    let seeds: Vec<_> = parts
+        .next()
+        .unwrap()
+        .split_once(":")
+        .unwrap()
+        .1
+        .trim()
+        .split_whitespace()
+        .map(|n| n.parse::<usize>().unwrap())
+        .collect();
+    let conversions: Vec<_> = parts
+        .map(|part| {
+            part.lines()
+                .skip(1)
+                .map(|line| {
+                    line.split_whitespace()
+                        .map(|n| n.parse::<usize>().unwrap())
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    let result = conversions
+        .iter()
+        .fold(seeds, |input, conversion| {
+            input
+                .iter()
+                .map(|&i| {
+                    let rule = conversion
+                        .iter()
+                        .filter(|&rule| i >= rule[1] && i <= rule[1] + rule[2])
+                        .next();
+                    match rule {
+                        Some(rule) => i + rule[0] - rule[1],
+                        None => i,
+                    }
+                })
+                .collect()
+        })
+        .into_iter()
+        .min()
+        .unwrap();
+    println!("res: {result}");
+}
