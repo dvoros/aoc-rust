bb9527f54ebbbb1d9c9a757a48e88966413369da
Daniel Voros <daniel.voros@gmail.com>
2023-12-11 08:53:45 +0100

    aoc 2023 / 11 / 2

---
+++ 2023/d11p2/src/main.rs
@@ -0,0 +1,62 @@
+use std::collections::HashSet;
+
+#[derive(Debug, PartialEq, PartialOrd)]
+enum Space {
+    Galaxy,
+    Empty,
+}
+
+fn main() {
+    let mut galaxies = HashSet::new();
+    let mx: Vec<Vec<_>> = include_str!("../input")
+        .trim()
+        .lines()
+        .enumerate()
+        .map(|(row, line)| {
+            line.as_bytes()
+                .iter()
+                .enumerate()
+                .map(|(col, b)| {
+                    let s = match b {
+                        b'.' => Space::Empty,
+                        b'#' => Space::Galaxy,
+                        _ => unreachable!(),
+                    };
+                    if s == Space::Galaxy {
+                        galaxies.insert((row, col));
+                    }
+                    s
+                })
+                .collect()
+        })
+        .collect();
+
+    let empty_rows: Vec<usize> = (0..mx.len())
+        .filter(|row| (0..mx[0].len()).all(|col| mx[*row][col] == Space::Empty))
+        .collect();
+    let empty_cols: Vec<usize> = (0..mx[0].len())
+        .filter(|col| (0..mx.len()).all(|row| mx[row][*col] == Space::Empty))
+        .collect();
+
+    let galaxies: HashSet<_> = galaxies
+        .iter()
+        .map(|(r, c)| {
+            (
+                r + 999999*empty_rows.iter().filter(|&er| er < r).count(),
+                c + 999999*empty_cols.iter().filter(|&ec| ec < c).count(),
+            )
+        })
+        .collect();
+
+    let sum: usize = galaxies
+        .iter()
+        .map(|g1| {
+            galaxies
+                .iter()
+                .filter(|g2| *g2 != g1)
+                .map(|g2| g2.0.abs_diff(g1.0) + g2.1.abs_diff(g1.1))
+                .sum::<usize>()
+        })
+        .sum();
+    println!("{}", sum / 2);
+}
